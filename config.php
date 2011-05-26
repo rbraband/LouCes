078067ed090822785e87a7b626cd3d6c3be3f270
Roland Braband <rbraband@web.de>
2011-05-26 23:33:51 +0200

    move ALICE_ID to config.php redefine alice regexp

--- config.php
+++ config.php
@@ -1,17 +1,25 @@
 <?php
 error_reporting(E_ALL);
-// configuration
+// bot configuration
 define('BOT_ALLY_NAME', 'YouAlliance');
 define('BOT_USER_NAME', 'BotName');
 define('BOT_EMAIL', 'botemail@whatever.com');
 define('BOT_PASSWORD', 'botpassword');
 define('BOT_OWNER', 'YourName');
 define('BOT_SERVER', 'http://prodgame12.lordofultima.com/25/'); // your world server!
-define('BOT_LANG','de'); // your prefered language for login  
-// prefix for commands to the bot
-define('PRE','!');
+define('BOT_LANG','de'); // your prefered language for login
+// database
+define('REDIS_CONNECTION', '/tmp/redis.socket'); // localhost:6379 or socket
+define('REDIS_NAMESPACE', 'lou:'); // use custom prefix on all keys
 // time settings
 date_default_timezone_set("Europe/Berlin"); // your default timezone
+// extension configuration
+define('ALICE_ID', ''); // need an published alicebot id  
+// prefix for commands to the bot
+define('PRE','!');
+//
+// after this, no changes needed!
+//
 // features like spam
 define('SPAMTTL', 15);
 define('POLLTRIP', 1000);
@@ -38,7 +46,4 @@ define('SYSTEM', '@');
 define('CHAT', 'CHAT');
 define('ALLIANCE', 'ALLIANCE');
 define('USER', 'USER');
-// database
-define('REDIS_CONNECTION', '/tmp/redis.socket'); // localhost:6379 or socket
-define('REDIS_NAMESPACE', 'lou:'); // use custom prefix on all keys
 ?>
