999712652c7292240c141d839e8fb8bbbe5b7bc2
Roland Braband <r.braband@web.de>
2012-08-22 23:50:29 +0200

    Change copyright

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 <?php
 /*
 PHPLoU_bot - an LoU bot writen in PHP
-Copyright (C) 2011 Roland Braband
+Copyright (C) 2012 Roland Braband / rbraband
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
 
