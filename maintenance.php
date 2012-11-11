2b50924c0569e1f1602e35571b38b514bd8def53
Roland Braband <rbraband@web.de>
2012-11-11 01:56:45 +0100

    Improve ll

--- maintenance.php
+++ maintenance.php
@@ -21,66 +21,78 @@ foreach($cache_keys as $cache) {
   $redis->del($cache);
 }
 echo count($cache_keys)." Cache Keys gelöscht!\n";
-
+$str_time = (string)time();
 /* maintenace */
 // check users with none points
-$users = $redis->HGETALL('users');
+$users = $redis->hGetALL('users');
 #$users = $redis->getKeys('user:[0-9]???:data');
 $ll = 0;
 $low = 0;
 if (is_array($users)) foreach($users as $user) {
   #echo $user . "\n";
-  $points = $redis->hget("user:{$user}:data", 'points');
+  $points = $redis->hGet("user:{$user}:data", 'points');
+  $alliance_id = $redis->hGet("user:{$user}:data",'alliance_id');
   $count++;
   if ($points == 0) {
-    $name = $redis->hget("user:{$user}:data", 'name');
+    $name = $redis->hGet("user:{$user}:data", 'name');
     echo "DELETE: {$user} | {$name}\n";
     $ll++;
     // delete from users
-    $redis->HDEL("users", $name);
+    $redis->hDel("users", $name);
 
     // fetch and delete aliase
     $aliase = $redis->sMembers("user:{$user}:alias");
     if (is_array($aliase)) foreach($aliase as $_alias) {
 
-      $redis->HDEL("aliase", $_alias);
+      $redis->hDel("aliase", $_alias);
     }
     // delete from continents
     $continent_keys = $redis->getKeys("user:{$user}:continent:*:data");
     if (is_array($continent_keys)) foreach($continent_keys as $_continent_key) {
       if (preg_match("/user:{$user}:continent:([0-9]*):data/sim", $_continent_key, $_matches)) {
-        $redis->HDEL("continent:{$_matches[1]}:residents", $name);
+        $redis->hDel("continent:{$_matches[1]}:residents", $name);
       }
     }
     // delete user from db
     $user_keys = $redis->getKeys("user:{$user}:*");
     if (is_array($user_keys)) foreach($user_keys as $_user_key) {
-      $redis->DEL($_user_key);
+      $redis->del($_user_key);
     } 
      
     continue;
   }
   elseif ($points <=3) $low++;
-  $user_cities = $redis->SMEMBERS("user:{$user}:cities");
+  $user_cities = $redis->sMembers("user:{$user}:cities");
   if (is_array($user_cities)) foreach($user_cities as $city) {
-    $city_id = $redis->HGET("cities", $city);
-    $city_data = $redis->HGETALL("city:{$city_id}:data");
+    $city_id = $redis->hGet("cities", $city);
+    $city_data = $redis->hGetALL("city:{$city_id}:data");
     if ($city_data['user_id'] != $user) {
-
       echo "DELETE User {$user} | city {$city}\n";
-      $redis->SREM("user:{$user}:cities", $city);
-      $redis->SREM("user:{$user}:continent:{$city_data['continent']}:cities", $city);
-      $redis->SADD("user:{$city_data['user_id']}:cities", $city);
-      $redis->SREM("user:{$city_data['user_id']}:continent:{$city_data['continent']}:cities", $city);
+      $redis->sRem("user:{$user}:cities", $city);
+      $redis->sRem("user:{$user}:continent:{$city_data['continent']}:cities", $city);
+      if ($city_data['user_id'] >= 1) {
+        $redis->sAdd("user:{$city_data['user_id']}:cities", $city);
+        $redis->sAdd("user:{$city_data['user_id']}:continent:{$city_data['continent']}:cities", $city);
+      }
+      $city_key = "city:{$city_id}";
+      $redis->hMset("{$city_key}:data", array(
+        'll_time'        => $str_time,
+        'll_name'        => $city_data['name'],
+        'll_state'       => $city_data['state'],
+        'll_points'      => $city_data['points'],
+        'll_category'    => $city_data['category'],
+        'll_user_id'     => $user,
+        'll_alliance_id' => $alliance_id
+      ));
     }
   }
 } echo $count. " User, " . $ll . " LL und " . $low . " <= 3 Punkte\n";
 
-$continents = $redis->SMEMBERS("continents");
+$continents = $redis->sMembers("continents");
 $settler_key = "settler";
 if (is_array($continents)) foreach ($continents as $continent) {
   $continent_key = "continent:{$continent}";
-  $redis->DEL("{$settler_key}:{$continent_key}:lawless");
+  $redis->del("{$settler_key}:{$continent_key}:lawless");
   echo "\nDELETE lawless on K{$continent}";
 }
 echo "\n";
@@ -88,15 +100,15 @@ echo "\n";
 /* found deprecated aliases */
 $aliases = $redis->hGetAll("aliase");
 if (is_array($aliases)) foreach ($aliases as $alias => $user) {
-  if (!$name = $redis->hget("user:{$user}:data", 'name')) {
+  if (!$name = $redis->hGet("user:{$user}:data", 'name')) {
     echo "Error: deprecated {$user}|{$alias}\n";
   } #else echo "Found: {$user}|{$name}\n";
 }
 
 /* found cities with empty points */
-$cities = $redis->HGETALL('cities');
+$cities = $redis->hGetALL('cities');
 if (is_array($cities)) foreach($cities as $city) {
-  $points = $redis->hget("city:{$city}:data", 'points');
+  $points = $redis->hGet("city:{$city}:data", 'points');
   if ($points == 0) echo "ERROR: city {$city} with empty points\n";
 }
 
