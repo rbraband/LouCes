7d4d083a5b373cefbe5eab012511ec984b5c9e01
Roland Braband <rbraband@web.de>
2013-02-12 10:44:14 +0100

    ~change to thread ~simplify code

--- bot_functions/doku.php
+++ bot_functions/doku.php
@@ -2,26 +2,42 @@
 global $bot;
 $bot->add_category('doku', array(), PUBLICY);
 // crons
-$bot->add_cron_event(Cron::HOURLY,                    // Cron key
+$bot->add_thread_event(Cron::HOURLY,                    // Cron key
                     "GetDokuUpdate",                  // command key
                     "LouBot_doku_update_cron",        // callback function
 function ($bot, $data) {
   global $redis;
   if (!$redis->status()) return;
+  update_doku($bot);
+}, 'doku');
+
+$bot->add_privmsg_hook("UpdateDoku",         		// command key
+                       "LouBot_update_doku", 		// callback function
+                       true,                 		// is a command PRE needet?
+                       '', 	                    // optional regex for key
+function ($bot, $data) {
+  global $redis;
+	if($bot->is_op_user($data['user'])) {
+    update_doku($bot);
+  } else $bot->add_privmsg("Ne Ne Ne!", $data['user']);
+}, 'operator');
   
-  $alliance_key = "alliance:{$bot->ally_id}";
-  $doku_key = "doku";
-  $str_time = (string)time();
-  $doku_name = "{$bot->bot_user_name}'s Welt";
-  if (!($forum_id = $redis->GET("{$doku_key}:{$alliance_key}:forum:id"))) {
-    $forum_id = $bot->forum->get_forum_id_by_name($doku_name, true);
-    $redis->SET("{$doku_key}:{$alliance_key}:forum:id", $forum_id);
-  }
-  $last_update = $redis->SMEMBERS('stats:DokuUpdate');
-  sort($last_update);
-  $last_update = end($last_update);
-  $dokus = load_doku();
-  if (is_array($dokus) && $forum_id) foreach ($dokus as $doku) {
+if (!function_exists('update_doku')) {
+  function update_doku($bot) {
+    global $redis;
+    $alliance_key = "alliance:{$bot->ally_id}";
+    $doku_key = "doku";
+    $str_time = (string)time();
+    $doku_name = "{$bot->bot_user_name}'s World";
+    if (!($forum_id = $redis->GET("{$doku_key}:{$alliance_key}:forum:id"))) {
+     $forum_id = $bot->forum->get_forum_id_by_name($doku_name, true);
+     $redis->SET("{$doku_key}:{$alliance_key}:forum:id", $forum_id);
+    }
+    $last_update = $redis->SMEMBERS('stats:DokuUpdate');
+    sort($last_update);
+    $last_update = end($last_update);
+    $dokus = load_doku();
+    if (is_array($dokus) && $bot->forum->exist_forum_id($forum_id)) foreach ($dokus as $doku) {
     $bot->lou->check(); if(empty($doku['title'])) continue;
     $thread_name = $doku['title'];
     $bot->log("Doku forum '{$thread_name}': start");
@@ -34,122 +50,89 @@ function ($bot, $data) {
       $force = true;
     }
     $update = version_compare($doku['version'], $_version, '>');
-    if ($thread_id && ($update || $force)) {
+      if ($bot->forum->exist_forum_thread_id($forum_id, $thread_id) && ($update || $force)) {
       // ** forum
-      $bot->forum->get_alliance_forum_posts($forum_id, $thread_id);
       $redis->SET("{$doku_key}:{$alliance_key}:forum:{$doku['key']}:version", $doku['version']);
+        $post = array();
       $_post_id = 0;
-      $post = array();
       foreach($doku['entrys'] as $entry) {
-        $post[$_post_id] = $entry;
-        $_post_id ++;
+          $post[$_post_id ++] = $entry;
       }
       $post_update = "[u]letztes Update:[/u] [i]{$doku['date']}[/i] | [u]Dokumentation:[/u] [spieler]{$doku['author']}[/spieler]";
+        // ** forum            
       foreach ($post as $_post_id_post => $_post) {
-        if (is_array($bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id_post])) {
-          if (!$bot->forum->edit_alliance_forum_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id_post]['post_id'], $_post)) $bot->log("Doku forum '{$thread_name}'/{$thread_id}/{$_post_id_post}: edit post error!");
+          if ($_id = $bot->forum->get_thread_post_by_num($forum_id, $thread_id, $_post_id_post)) {
+            if (!$bot->forum->edit_alliance_forum_post($forum_id, $thread_id, $_id, $_post)) {
+              $bot->log("Doku forum '{$thread_name}'/{$thread_id}/{$_post_id_post}: edit post error!");
+              $bot->debug($_post);
+              $error = 3;
+            }
         } else {
-          if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $_post)) $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
+            if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $_post)) {
+              $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
+              $bot->debug($_post);
+              $error = 3;
         }
       }
-        
-      if ($update && count($bot->forum->posts[$forum_id][$thread_id]['data']) >= count($post)) {
-        $bot->log("Doku forum '{$thread_name}': update({$doku['date']}) v{$doku['version']} posts:" . count($bot->forum->posts[$forum_id][$thread_id]['data']) . '|' . count($post));
-        for($idx = count($post); $idx <= count($bot->forum->posts[$forum_id][$thread_id]['data']); $idx++) {
-          $bot->forum->delete_alliance_forum_threads_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$idx]['post_id']);
         }
-        if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $post_update)) $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
+        $_posts_count = $bot->forum->get_thread_post_count($forum_id, $thread_id);  
+        if ($update && $_posts_count >= count($post)) {
+        $bot->log("Doku forum '{$thread_name}': update({$doku['date']}) v{$doku['version']} posts:" . count($bot->forum->posts[$forum_id][$thread_id]['data']) . '|' . count($post));
+          for($idx = count($post); $idx <= $_posts_count; $idx++) {
+            $bot->forum->delete_alliance_forum_threads_post($forum_id, $thread_id, $bot->forum->get_thread_post_by_num($forum_id, $thread_id, $idx));
+          }
+          if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $post_update)) {
+            $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
+            $bot->debug($post_update);
+            $error = 3;
+        }
       } else {
         $post[$_post_id] = $post_update;
         $bot->log("Doku forum '{$thread_name}': update({$doku['date']}) v{$doku['version']} posts:" . count($bot->forum->posts[$forum_id][$thread_id]['data']) . '|' . count($post));
-        for($idx = count($post); $idx <= count($bot->forum->posts[$forum_id][$thread_id]['data']); $idx++) {
-          $bot->forum->delete_alliance_forum_threads_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$idx]['post_id']);
+          for($idx = count($post); $idx <= $_posts_count; $idx++) {
+            $bot->forum->delete_alliance_forum_threads_post($forum_id, $thread_id, $bot->forum->get_thread_post_by_num($forum_id, $thread_id, $idx));
+          }
+          if ($_id = $bot->forum->get_thread_post_by_num($forum_id, $thread_id, $_post_id)) {
+            if (!$bot->forum->edit_alliance_forum_post($forum_id, $thread_id, $_id, $post[$_post_id])) {
+              $bot->log("Doku forum '{$thread_name}'/{$thread_id}/{$_post_id}: edit post error!");
+              $bot->debug($post[$_post_id]);
+              $error = 3;
         }
-        if (is_array($bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id])) {
-          if (!$bot->forum->edit_alliance_forum_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id]['post_id'], $post[$_post_id])) $bot->log("Doku forum '{$thread_name}'/{$thread_id}/{$_post_id}: edit post error!");
         } else {
-          if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $post[$_post_id])) $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
+            if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $post[$_post_id])) {
+              $bot->log("Doku forum {$thread_name}/{$thread_id}: create post error!");
+              $bot->debug($post[$_post_id]);
+              $error = 3;
         }
       }
     }
-  } else $bot->log("Doku error: no forum '{$doku_name}'");
-}, 'doku');
+      }
+    } else {
+      $error = 4;
+      $bot->log("Doku error: no forum '{$doku_name}'");
+      $redis->del("{$doku_key}:{$alliance_key}:forum:id");
+    }
+  }
+}
 
-
-//callbacks
-$bot->add_privmsg_hook("ReloadDoku",           // command key
-                       "LouBot_reload_doku",   // callback function
+$bot->add_privmsg_hook("ReloadDokuForum",             // command key
+                       "LouBot_reload_duko_forum",    // callback function
                        true,                   // is a command PRE needet?
                        '',                     // optional regex for key
 function ($bot, $data) {
   global $redis;
+  if (!$redis->status()) return;
   if($bot->is_op_user($data['user'])) {
-    if (!$redis->status()) return;
-  
     $alliance_key = "alliance:{$bot->ally_id}";
     $doku_key = "doku";
-    $str_time = (string)time();
-    $doku_name = "{$bot->bot_user_name}'s Welt";
-    if (!($forum_id = $redis->GET("{$doku_key}:{$alliance_key}:forum:id"))) {
-      $forum_id = $bot->forum->get_forum_id_by_name($doku_name, true);
-      $redis->SET("{$doku_key}:{$alliance_key}:forum:id", $forum_id);
+    $doku_key_keys = $redis->getKeys("{$doku_key}:{$alliance_key}:forum:*");
+    if (!empty($doku_key_keys)) foreach($doku_key_keys as $doku_key_key) {
+      $redis->del("{$doku_key_key}");
     }
-    $last_update = $redis->SMEMBERS('stats:DokuUpdate');
-    sort($last_update);
-    $last_update = end($last_update);
-    $dokus = load_doku();
-    if (is_array($dokus) && $forum_id) foreach ($dokus as $doku) {
-      $bot->lou->check(); if(empty($doku['title'])) continue;
-      $thread_name = $doku['title'];
-      $bot->log("Doku forum '{$thread_name}': start");
-      if (!($thread_id = $redis->GET("{$doku_key}:{$alliance_key}:forum:{$doku['key']}:id"))) {
-        $thread_id = $bot->forum->get_forum_thread_id_by_title($forum_id, $thread_name, true);
-        $redis->SET("{$doku_key}:{$alliance_key}:forum:{$doku['key']}:id", $thread_id);
-      }
-      $force = false;
-      if (!($_version = $redis->GET("{$doku_key}:{$alliance_key}:forum:{$doku['key']}:version"))) {
-        $force = true;
-      }
-      $update = version_compare($doku['version'], $_version, '>');
-      if ($thread_id && ($update || $force)) {
-        // ** forum
-        $bot->forum->get_alliance_forum_posts($forum_id, $thread_id);
-        $redis->SET("{$doku_key}:{$alliance_key}:forum:{$doku['key']}:version", $doku['version']);
-        $_post_id = 0;
-        $post = array();
-        foreach($doku['entrys'] as $entry) {
-          $post[$_post_id] = $entry;
-          $_post_id ++;
-        }
-        $post_update = "[u]letztes Update:[/u] [i]{$doku['date']}[/i] | [u]Dokumentation:[/u] [spieler]{$doku['author']}[/spieler]";
-        foreach ($post as $_post_id_post => $_post) {
-          if (is_array($bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id_post])) {
-            if (!$bot->forum->edit_alliance_forum_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id_post]['post_id'], $_post)) $bot->log("Doku forum '{$thread_name}'/{$thread_id}/{$_post_id_post}: edit post error!");
-          } else {
-            if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $_post)) $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
-          }
-        }
-          
-        if ($update && count($bot->forum->posts[$forum_id][$thread_id]['data']) >= count($post)) {
-          $bot->log("Doku forum '{$thread_name}': update({$doku['date']}) v{$doku['version']} posts:" . count($bot->forum->posts[$forum_id][$thread_id]['data']) . '|' . count($post));
-          for($idx = count($post); $idx <= count($bot->forum->posts[$forum_id][$thread_id]['data']); $idx++) {
-            $bot->forum->delete_alliance_forum_threads_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$idx]['post_id']);
-          }
-          if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $post_update)) $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
-        } else {
-          $post[$_post_id] = $post_update;
-          $bot->log("Doku forum '{$thread_name}': update({$doku['date']}) v{$doku['version']} posts:" . count($bot->forum->posts[$forum_id][$thread_id]['data']) . '|' . count($post));
-          for($idx = count($post); $idx <= count($bot->forum->posts[$forum_id][$thread_id]['data']); $idx++) {
-            $bot->forum->delete_alliance_forum_threads_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$idx]['post_id']);
-          }
-          if (is_array($bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id])) {
-            if (!$bot->forum->edit_alliance_forum_post($forum_id, $thread_id, $bot->forum->posts[$forum_id][$thread_id]['data'][$_post_id]['post_id'], $post[$_post_id])) $bot->log("Doku forum '{$thread_name}'/{$thread_id}/{$_post_id}: edit post error!");
-          } else {
-            if (!$bot->forum->create_alliance_forum_post($forum_id, $thread_id, $post[$_post_id])) $bot->log("Doku forum '{$thread_name}'/{$thread_id}: create post error!");
-          }
-        }
-      }
-    } else $bot->add_privmsg("Doku error: no forum '{$doku_name}'");
+    $doku_name = "{$bot->bot_user_name}'s World";
+    $bot->add_privmsg("Step1# ".$doku_name." REDIS ids deleted!", $data['user']);
+    $bot->call_event(array('type' => TICK, 'name' => Cron::HOURLY), 'LouBot_doku_update_cron');
+    $bot->add_privmsg("Step2# ".$doku_name." reload done!", $data['user']);
   } else $bot->add_privmsg("Ne Ne Ne!", $data['user']);
 }, 'operator');
 
