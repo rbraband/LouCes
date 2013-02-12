27ee8a1cf27071483a3f88783721de284af2d9b1
Roland Braband <rbraband@web.de>
2013-02-12 08:24:09 +0100

    ~formatting

--- cron.php
+++ cron.php
@@ -18,16 +18,16 @@ class Cron implements SplSubject {
   protected $observers= array ();
   
   const TICK0    = 'cron.tick_0';
-  const TICK1   = 'cron.tick_1';
-  const TICK5   = 'cron.tick_5';
+  const TICK1    = 'cron.tick_1';
+  const TICK5    = 'cron.tick_5';
   const TICK10   = 'cron.tick_10';
   const TICK15   = 'cron.tick_15';
   const TICK20   = 'cron.tick_20';
   const TICK30   = 'cron.tick_30';
   const HOURLY   = 'cron.hourly';
-  const DAILY   = 'cron.daily';
+  const DAILY    = 'cron.daily';
   const WEEKLY   = 'cron.weekly';
-  const MONTHLY = 'cron.monthly';
+  const MONTHLY  = 'cron.monthly';
   const YEARLY   = 'cron.yearly';
   
   private $crons = array(
@@ -35,17 +35,17 @@ class Cron implements SplSubject {
     array('cron.daily',   'Gis',  1),
     array('cron.weekly',  'wGis', 1),
     array('cron.monthly', 'jGis', 100001),
-    array('cron.yearly',   'zGis', 1)
+    array('cron.yearly',  'zGis', 1)
   );
   
   private $ticks = array(
     array('cron.tick_0',  's',   10),
-    array('cron.tick_1',   'is',      100),
-    array('cron.tick_5',   'is',      500),
-    array('cron.tick_10', 'is',      1000),
-    array('cron.tick_15', 'is',      1500),
-    array('cron.tick_20', 'is',      2000),
-    array('cron.tick_30', 'is',      3000),
+    array('cron.tick_1',  'is',  100),
+    array('cron.tick_5',  'is',  500),
+    array('cron.tick_10', 'is',  1000),
+    array('cron.tick_15', 'is',  1500),
+    array('cron.tick_20', 'is',  2000),
+    array('cron.tick_30', 'is',  3000),
   );
 
   static function factory() {
@@ -74,7 +74,7 @@ class Cron implements SplSubject {
     }
     return $return;
   }
-
+	
   private function kick($type, $event) {
     $this->note = array('type' => $type,
                         'name' => $event,
